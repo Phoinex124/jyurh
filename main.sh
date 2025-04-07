3051e4c835ef950213689dce529153f4820a4e01
potato. <marottamatteo73@gmail.com>
2025-04-07 23:44:20 +0000

    Edit main.sh

--- main.sh
+++ main.sh
@@ -2,6 +2,9 @@
 echo starting...
 cd bungee
 
+MTOD="${MTOD:-@atypicalpotato on youtube}"
+
 sed -i 's/${SERVER}/'"$SERVER"'/g' config.yml
+sed -i 's/${MTOD}/'"$MTOD"'/g' listeners.yml
 
 java -Xmx1024M -Xms1024M -jar bungee.jar
