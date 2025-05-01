ecaceee9cc647245820b1c6255e9820e80a32524
potato. <marottamatteo73@gmail.com>
2025-05-01 23:22:15 +0000

    Edit main.sh

--- main.sh
+++ main.sh
@@ -3,6 +3,7 @@ echo starting...
 cd bungee
 
 MTOD="${MTOD:-'@atypicalpotato on youtube'}"
+NAME="${NAME:-Potato Server}"
 
 sed -i 's/${SERVER}/'"$SERVER"'/g' config.yml
 
@@ -10,6 +11,12 @@ cd plugins
 cd EaglercraftXBungee
 
 sed -i 's/${MTOD}/'"$MTOD"'/g' listeners.yml
+
+cd /
+cd bungee
+cd web
+sed -i 's/${NAME}/'"$NAME"'/g' wasm.html
+sed -i 's/${NAME}/'"$NAME"'/g' game.html
 
 cd /
 cd bungee
